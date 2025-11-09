615f95253bd6325b880678a0fe0c394042528d18
Kelvin Barsana <kmbarsana@gmail.com>
2025-11-09 18:35:12 +0800

    contact us page position adjustments (enlarged map to fill empty space)

--- resources/js/pages/unauth/contactUs/index.tsx
+++ resources/js/pages/unauth/contactUs/index.tsx
@@ -56,45 +56,45 @@ export default function Index({ contact }: { contact: WebsiteAppearanceForm }) {
         <AppLayoutHeaderCustomer page="Contact Us">
             <Head title="Contact Us" />
             <form onSubmit={handleSubmit}>
-                <div className="grid grid-cols-3 gap-3 md:gap-10">
-                    <div className="col-span-3 flex w-full flex-col gap-3 md:order-2 md:col-span-2">
-                        <h2 className="text-foreground text-xl">Your reservations?</h2>
-                        <p className="text-muted-foreground text-sm">
-                            No worries! We'll send you your active reservations. Just provide your
-                            email below.
-                        </p>
-                        <div className="flex flex-row gap-3">
-                            <div className="flex-1">
-                                <Input
-                                    id="email"
-                                    type="text"
-                                    autoFocus
-                                    autoComplete="email"
-                                    value={data.email}
-                                    onChange={(e) => setData('email', e.target.value)}
-                                    placeholder="Enter Email"
-                                    disabled={processing}
-                                />
-                                <InputError
-                                    message={zodErrors.email || errors.email}
-                                    className="ml-3"
-                                />
+                <div className="flex w-auto flex-col gap-3 py-6">
+                    <div className="grid grid-cols-3 gap-5">
+                        <div className="col-span-3 flex flex-col gap-3 md:col-span-1">
+                            <div className="col-span-3 flex w-full flex-col gap-3 md:col-span-1">
+                                <h2 className="text-foreground text-xl">Your reservations?</h2>
+                                <p className="text-muted-foreground text-sm">
+                                    No worries! We'll send you your active reservations. Just
+                                    provide your email below.
+                                </p>
+                                <div className="flex flex-row gap-3">
+                                    <div className="flex-1">
+                                        <Input
+                                            id="email"
+                                            type="text"
+                                            autoFocus
+                                            autoComplete="email"
+                                            value={data.email}
+                                            onChange={(e) => setData('email', e.target.value)}
+                                            placeholder="Enter Email"
+                                            disabled={processing}
+                                        />
+                                        <InputError
+                                            message={zodErrors.email || errors.email}
+                                            className="ml-3"
+                                        />
+                                    </div>
+                                    <Button
+                                        variant="makeshiftDefault"
+                                        size="makeshiftXl"
+                                        disabled={processing}
+                                        type="submit"
+                                        className="md:w-fit"
+                                    >
+                                        Send
+                                    </Button>
+                                </div>
                             </div>
-                            <Button
-                                variant="makeshiftDefault"
-                                size="makeshiftXl"
-                                disabled={processing}
-                                type="submit"
-                                className="md:w-fit"
-                            >
-                                Send
-                            </Button>
-                        </div>
-                    </div>
-                    <Separator className="col-span-3 my-4 md:hidden" />
-                    <div className="col-span-3 flex w-auto flex-col gap-3 md:order-1 md:col-span-1">
-                        <h2 className="text-foreground text-xl">Stay in touch</h2>
-                        <div className="flex flex-col gap-3">
+                            <Separator className="col-span-3 my-4" />
+                            <h2 className="text-foreground text-xl">Stay in touch</h2>
                             <div className="flex flex-col gap-1">
                                 <h3 className="text-foreground text-md">Email</h3>
                                 <p className="text-muted-foreground text-sm">
@@ -113,19 +113,19 @@ export default function Index({ contact }: { contact: WebsiteAppearanceForm }) {
                                     {contact.siteAddress}
                                 </p>
                             </div>
-                            <Card className="shadow-l mt-3 w-full overflow-hidden rounded-2xl p-0">
-                                <CardContent className="p-0">
-                                    <iframe
-                                        title="Google Map"
-                                        src={contact.siteGoogleMap}
-                                        className="h-96 w-full border-0"
-                                        allowFullScreen
-                                        loading="lazy"
-                                        referrerPolicy="no-referrer-when-downgrade"
-                                    />
-                                </CardContent>
-                            </Card>
                         </div>
+                        <Card className="shadow-l col-span-3 w-full overflow-hidden rounded-2xl p-0 md:col-span-2">
+                            <CardContent className="p-0">
+                                <iframe
+                                    title="Google Map"
+                                    src={contact.siteGoogleMap}
+                                    className="h-102 w-full border-0"
+                                    allowFullScreen
+                                    loading="lazy"
+                                    referrerPolicy="no-referrer-when-downgrade"
+                                />
+                            </CardContent>
+                        </Card>
                     </div>
                 </div>
             </form>
